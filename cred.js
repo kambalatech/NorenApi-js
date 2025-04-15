670a9577dca4d222dabd43b55a381a67b2e84dd6
annappa.achar <annappa.achar@kambala.co.in>
2025-04-15 15:23:25 +0530

    Eod chart added

--- cred.js
+++ cred.js
@@ -1,8 +1,8 @@
 module.exports.authparams = {
-'userid'   : 'USER',
-'password' : 'Abc123',
-'twoFA'    : '01011970',
-'vendor_code' : 'VC',
-'api_secret' : 'your secret',
-'imei'       : 'machine id'
+'userid'   : 'ANNAPPA',
+'password' : 'Test@123',
+'twoFA'    : '1999',
+'vendor_code' : 'NOREN_API',
+'api_secret' : 'Asfghd*G',
+'imei'       : 'V101'
 }
--- request_example.js
+++ request_example.js
@@ -15,27 +15,33 @@ api.login(authparams)
         let params = {
             'exchange'  : 'NSE',
             'token'     :  '22',
-            'starttime' :  '1639280700',           
-            'interval'  : '5'
+            'starttime' :  '1736394345',           
+            'interval'  : '5',
+            'endtime':"1737324034",
+            'tsym':'ACC-EQ'
             
         };
         api.get_time_price_series(params).then((reply) => { console.log(reply); });
 
-        api.forgot_passwordOTP('NIKHESHP','AAAAA1234A').then((reply) => { console.log(reply); });
-        return;
-        //search scrip example
-        api.searchscrip('NFO', 'NIFTY DEC CE').then((reply) => { console.log(reply); });
+        api.get_daily_price_series(params).then((reply) => { console.log(reply); });
+
+    //    api.forgot_passwordOTP('NIKHESHP','AAAAA1234A').then((reply) => { console.log(reply); });
+    //    return;
+
+    //     // search scrip example
+    //    api.searchscrip('NFO', 'NIFTY DEC CE').then((reply) => { console.log(reply); });
         
-        //get quote example
-        api.get_quotes('NSE', '22').then((reply) => { console.log(reply); });
+    //     // get quote example
+    //    api.get_quotes('NSE', '22').then((reply) => { console.log(reply); });
 
-        api.get_orderbook().then((reply) => { console.log(reply); });
+    //    api.get_orderbook().then((reply) => { console.log(reply); });
 
-        api.get_tradebook().then((reply) => { console.log(reply); });
+    //    api.get_tradebook().then((reply) => { console.log(reply); });
 
-        api.get_holdings().then((reply) => { console.log(reply); });
+    //    api.get_holdings().then((reply) => { console.log(reply); });
         
-        api.get_positions().then((reply) => { console.log(reply); });
+    //    api.get_positions().then((reply) => { console.log(reply); });
+        
         
     }).catch((err) => {
         console.error(err);
